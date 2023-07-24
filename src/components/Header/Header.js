2a69489d20bb65e26508313dfb3d12828d5da6ba
JagsP23 <pullapuramjr@gmail.com>
2023-07-24 15:01:39 -0400

    added components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <header className="header">
+        {/* <img src={logo} alt="logo" /> */}
+        <h1>Investment Calculator</h1>
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/UserInput/UserInput.js
@@ -0,0 +1,60 @@
+function UserInput(){
+
+    function submitHandler(event){
+        event.preventDefault();
+
+    }
+
+    function resetHandler(event){
+            event.preventDefault();
+    }
+
+    function inputChangeHandler(input, value){
+
+    }
+
+
+
+    return(
+        <form onSubmit={submitHandler} className="form">
+
+         <div className="input-group">
+          <p>
+            <label htmlFor="current-savings">Current Savings ($)</label>
+            <input onChange={(event)=> inputChangeHandler('current-savings', event.target.value)} type="number" id="current-savings" />
+          </p>
+          <p>
+            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+            <input onChange={(event)=> inputChangeHandler('yearly-contribution', event.target.value)}
+            type="number" id="yearly-contribution" />
+          </p>
+        </div>
+
+        <div className="input-group">
+          <p>
+            <label htmlFor="expected-return">
+              Expected Interest (%, per year)
+            </label>
+            <input onChange={(event)=> inputChangeHandler('expected-return', event.target.value)}
+            type="number" id="expected-return" />
+          </p>
+          <p>
+            <label htmlFor="duration">Investment Duration (years)</label>
+            <input onChange={(event)=> inputChangeHandler('duration', event.target.value)}
+            type="number" id="duration" />
+          </p>
+        </div>
+
+        <p className="actions">
+          <button onSubmit={resetHandler} type="reset" className="buttonAlt">
+            Reset
+          </button>
+          <button type="submit" className="button">
+            Calculate
+          </button>
+        </p>
+      </form>
+    )
+}
+
+export default UserInput;
